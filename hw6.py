40e84d1050f48207e4799e648b013553c64e7f52
Choi Seokjun <Choi.Seokjun@outlook.com>
2023-03-23 02:21:23 -0700

    hw6: simulated setting, with predictive / error analysis

--- hw6.py
+++ hw6.py
@@ -46,12 +46,12 @@ class GaussianMarkovRandomField_simulator:
         # is it the best way?
         return sample
     
-    def get_quick_rectangular_grid(self, vertical_knot_num, horizontal_knot_num, knot_gap=1):
-        grid = []
-        for i in range(vertical_knot_num):
-            for j in range(horizontal_knot_num):
-                grid.append((i*knot_gap,j*knot_gap))
-        return grid
+def get_quick_rectangular_grid(vertical_knot_num, horizontal_knot_num, knot_gap:float = 1):
+    grid = []
+    for i in range(vertical_knot_num):
+        for j in range(horizontal_knot_num):
+            grid.append((i*knot_gap,j*knot_gap))
+    return grid
         
 if __name__=="__main__":
     # simulate data from process convolution based on GMRF
@@ -65,7 +65,7 @@ if __name__=="__main__":
 
     lambda_X = 1
     gmrf_sample = gmrf_simulator_inst.draw_one_realization(lambda_scaler=lambda_X)
-    gmrf_grid = gmrf_simulator_inst.get_quick_rectangular_grid(knots_num_v, knots_num_h, 1/knots_num_v)
+    gmrf_grid = get_quick_rectangular_grid(knots_num_v, knots_num_h, 1/10)
 
     y_loc = [(np_random_inst_on_main.random(), np_random_inst_on_main.random()) for _ in range(300)]
     K_ker_mat = generate_kernel_matrix(y_loc, gmrf_grid, v_smoothness = 1, ker_range=0.4)
@@ -73,10 +73,12 @@ if __name__=="__main__":
     y_val = K_ker_mat @ gmrf_sample + np_random_inst_on_main.multivariate_normal(np.zeros(K_ker_mat.shape[0]), np.eye(K_ker_mat.shape[0])/lambda_y)
 
     plt.scatter(*zip(*gmrf_grid), c=gmrf_sample, marker=5)
-    plt.scatter(*zip(*y_loc), c=y_val, s=1.5)
+    sct_true = plt.scatter(*zip(*y_loc), c=y_val, s=1.5)
+    handles_true, labels_true = sct_true.legend_elements(prop="colors", alpha=0.6)
+    plt.legend(handles_true, labels_true, loc=2)
     plt.show()
 
-
+    # ===
 
     # fit reversely
     from pyBayes.MCMC_Core import MCMC_Gibbs, MCMC_Diag
@@ -146,9 +148,26 @@ if __name__=="__main__":
             new_sample[2] = new_lambda_Y
             return new_sample
     
-    gibbs_inst = fitter([np.zeros(knots_num_v*knots_num_h), 1, 1], y_val, K_ker_mat, gmrf_simulator_inst.W_precision, 
+    true_setting = False
+    if true_setting:
+        fit_knots_num_v = 10
+        fit_knots_num_h = 10
+        fit_grid = get_quick_rectangular_grid(fit_knots_num_v, fit_knots_num_h, 1/fit_knots_num_h)
+        fit_K_ker_mat = generate_kernel_matrix(y_loc, fit_grid, 1, 0.4)
+        fit_W = gmrf_simulator_inst.W_precision
+    else:
+        fit_knots_num_v = 15
+        fit_knots_num_h = 15
+        fit_grid = get_quick_rectangular_grid(fit_knots_num_v, fit_knots_num_h, 1/fit_knots_num_h)
+        fit_K_ker_mat = generate_kernel_matrix(y_loc, fit_grid, 1, 0.4)
+        fit_simul_inst = GaussianMarkovRandomField_simulator(20230323)
+        fit_simul_inst.set_rectangular_grid(fit_knots_num_v, fit_knots_num_h)
+        fit_W = fit_simul_inst.W_precision
+    
+
+    gibbs_inst = fitter([np.zeros(fit_knots_num_v*fit_knots_num_h), 1, 1], y_val, fit_K_ker_mat, fit_W,
                         0.01, 0.01, 0.01, 0.01, 20230322)
-    gibbs_inst.generate_samples(1000)
+    gibbs_inst.generate_samples(500)
     X_samples = [x[0] for x in gibbs_inst.MC_sample]
     lambda_samples = [[x[1], x[2]] for x in gibbs_inst.MC_sample]
     diag_inst2 = MCMC_Diag()
@@ -162,11 +181,59 @@ if __name__=="__main__":
     diag_inst1 = MCMC_Diag()
     diag_inst1.set_mc_samples_from_list(X_samples)
     diag_inst1.set_variable_names(["x"+str(i) for i in range(1, knots_num_v*knots_num_h+1)])
-    diag_inst1.show_traceplot((1,3), [0,1,2])
-    diag_inst1.show_hist((1,3), [0,1,2])
-    diag_inst1.show_acf(30, (1,3), [0,1,2])
+    diag_inst1.burnin(300)
+    # diag_inst1.show_traceplot((1,3), [0,1,2])
+    # diag_inst1.show_hist((1,3), [0,1,2])
+    # diag_inst1.show_acf(30, (1,3), [0,1,2])
     
+    posterior_x_mean = diag_inst1.get_sample_mean()
+    posterior_x_var = diag_inst1.get_sample_var()
 
+    # ===
 
-
+    s_new_pts = get_quick_rectangular_grid(31, 31, 1/31)
+    K_for_krigging = generate_kernel_matrix(s_new_pts, fit_grid, v_smoothness = 1, ker_range=0.4)
+    pred_sample_vec = []
+    for x, lamb_vec in zip(diag_inst1.MC_sample, diag_inst2.MC_sample):
+        pred_sample = K_for_krigging @ x + np_random_inst_on_main.multivariate_normal(np.zeros(len(s_new_pts)), np.eye(len(s_new_pts))/lamb_vec[1])
+        pred_sample_vec.append(pred_sample)
+    
+    pred_inst1 = MCMC_Diag()
+    pred_inst1.set_mc_samples_from_list(pred_sample_vec)
+    pred_y_mean = pred_inst1.get_sample_mean()
+    pred_y_var = pred_inst1.get_sample_var()
         
+    plt.scatter(*zip(*fit_grid), c=posterior_x_mean, marker=5)
+    sct_m = plt.scatter(*zip(*s_new_pts), c=pred_y_mean, s=1.5)
+    handles_m, labels_m = sct_m.legend_elements(prop="colors", alpha=0.6)
+    plt.legend(handles_m, labels_m)
+    plt.title("predictive mean")
+    plt.show()
+
+    # plt.scatter(*zip(*fit_grid), c=posterior_x_var, marker=5)
+    sct_v = plt.scatter(*zip(*s_new_pts), c=pred_y_var, s=1.5)
+    handles_v, labels_v = sct_v.legend_elements(prop="colors", alpha=0.6)
+    plt.legend(handles_v, labels_v)
+    plt.title("predictive variance")
+    plt.show()
+    
+    # ===
+
+    s_original_pts = y_loc
+    K_for_krigging2 = generate_kernel_matrix(y_loc, fit_grid, v_smoothness = 1, ker_range=0.4)
+    pred_sample_vec2 = []
+    for x, lamb_vec in zip(diag_inst1.MC_sample, diag_inst2.MC_sample):
+        pred_sample2 = K_for_krigging2 @ x + np_random_inst_on_main.multivariate_normal(np.zeros(len(s_original_pts)), np.eye(len(s_original_pts))/lamb_vec[1])
+        pred_sample_vec2.append(pred_sample2)
+    
+    pred_inst2 = MCMC_Diag()
+    pred_inst2.set_mc_samples_from_list(pred_sample_vec2)
+    pred_y_mean2 = pred_inst2.get_sample_mean()
+    pred_y_var2 = pred_inst2.get_sample_var()
+        
+    sct_e = plt.scatter(*zip(*s_original_pts), c=(np.array(y_val)-np.array(pred_y_mean2))**2, s=1.5)
+    handles_e, labels_e = sct_e.legend_elements(prop="colors", alpha=0.6)
+    plt.legend(handles_e, labels_e)
+    plt.title("squared error")
+    plt.show()
+
